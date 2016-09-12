55583abc8730096eebe4dc02627bdc2c0db903e6
David Maier <david.maier@couchbase.com>
2016-09-12 19:07:01 +0100

    Externalized a date conversion function to the helper

--- helper.js
+++ helper.js
@@ -11,12 +11,44 @@ function _isDefined(obj)
     return true;
 }
 
+
+function _twoDigVal(val) {
+
+	if ((''+val).length == 1) val = '0' + val;
+	return val;
+}
+
+
+function _genDateStr(ts) {
+
+	var date = new Date(ts);
+	  
+	var year = date.getUTCFullYear();
+	var month = _twoDigVal(date.getUTCMonth()+1);
+	var day = _twoDigVal(date.getUTCDate());
+	var hour = _twoDigVal(date.getUTCHours());
+	var min = _twoDigVal(date.getUTCMinutes());
+	
+	var dStr = year + "-" + month + "-" + day + "T" + hour + ":" + min;
+	return dStr;
+}
+
+
+
+
+
+
 //Module exports
 module.exports = {
     
     isDefined : function(obj) {
     
         return _isDefined(obj);
+    },
+
+    genDateStr : function(ts) {
+
+	return _genDateStr(ts);	     
     }
     
 };
